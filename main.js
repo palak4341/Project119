03b15bf5cf185b4f97155309480ddfd7a99dc3a6
palak4341 <105328703+palak4341@users.noreply.github.com>
2024-01-19 20:37:31 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ Webcam.attach(camera);
 
 function take_snapshot(){
     Webcam.snap(function(data_uri){
-        document.getElementById("results").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
